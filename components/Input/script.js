b8bcb74d781e26e1583a3616187e9a721c29e060
BISINIGISHIVANI <90177603+BISINIGISHIVANI@users.noreply.github.com>
2022-02-04 11:03:09 +0530

    feat:add image and Input

---
+++ components/Input/script.js
@@ -0,0 +1,23 @@
+var userInput = document.querySelector("#phone-number")
+var submitBtn = document.querySelector("#submit")
+var output = document.querySelector("#output")
+var label = document.querySelector("#label-pwd")
+
+const validInputCheck = () => {
+    if (userInput.value.length != 10) {
+        output.style.color = `red`;
+        label.style.color = `red`;
+        userInput.style.border = `1px solid red`;
+        userInput.style.outline = `none`;
+        var inValidOutput = `please enter 10 digit valid mobile number`;
+        output.innerHTML = inValidOutput;
+    } else {
+        output.style.color = `green`;
+        label.style.color = `green`;
+        userInput.style.border = `1px solid green`;
+        userInput.style.outline = `none`;
+        var validOutput = `you entered mobile number correctly`;
+        output.innerHTML = validOutput;
+    }
+}
+submitBtn.addEventListener("click", validInputCheck);
