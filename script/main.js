8ab47d0eae319036484f3fe714e029f8430bbd46
BISINIGISHIVANI <90177603+BISINIGISHIVANI@users.noreply.github.com>
2022-02-21 11:22:30 +0530

    style: update links

--- script/main.js
+++ script/main.js
@@ -1,30 +1,3 @@
-// Clipboard
-const clipBtn=document.querySelector("#clip-btn")
-const copyText=document.querySelector("#clip-input")
-
-const clipAction=()=>{
-    copyText.select();
-    navigator.clipboard.writeText(copyText.value);
-    alert("Copied text:"+copyText.value);
-}
-clipBtn.addEventListener('click',clipAction);
-
-//Sidebar 
-const menubtn=document.querySelector("#menu-bar");
-const sidemenu=document.querySelector("#open-sidemenu");
-const closeSidebar=document.querySelector("#close-btn");
-
-const openSidebar=()=>{
-    sidemenu.style.display="block";
-    closeSidebar.style.display=`block`;
-    sidemenu.style.marginTop=`2rem`;
-}
-const closeSide=()=>{
-    sidemenu.style.display="none";
-}
-menubtn.addEventListener("click",openSidebar)
-
-closeSidebar.addEventListener("click",closeSide)
 // badge hide 
 const btnInput=document.querySelector("#btn-input");
 const hideBadge=()=>{
@@ -76,5 +49,4 @@ const output=document.querySelector("#value-range");
 output.innerHTML=slider.value;
 slider.oninput = function() {
     output.innerHTML = this.value;}
-
 
